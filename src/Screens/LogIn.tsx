3d6fc25f4ecbbb2efae0f01e7495bb5dce7c5a21
MartinB04 <martin.bibiano4@outlook.com>
2024-10-13 12:48:53 -0600

    Change: Inicio de sesion agregado.
    [LogIn.tsx]: Actualizado.
    *Ahora permite iniciar sesion al ingresar username y password.
    *Si las credenciales son incorrectas muestra un mensaje de alerta.
    *Si las credenciales son correctas, se navega al BottomTabNavigator..

--- src/Screens/LogIn.tsx
+++ src/Screens/LogIn.tsx
@@ -1,16 +1,39 @@
 import React, { useState } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Alert } from 'react-native'
 import { ScrollView, TextInput, TouchableOpacity } from 'react-native-gesture-handler'
 
 import { useNavigation } from '@react-navigation/native';
 import { SignIn } from './SignIn';
 import { stylesAppTheme } from '../Theme/AppTheme';
-import { Home } from './Home';
 
 
 export const LogIn = () => {
 
   const navigation = useNavigation();
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navegacion = () => {
+    navigation.navigate("BottomTabNavigator");
+  }
+
+  const IniciarSesion = () => {
+    /* fetch('https://kuramadev.com/Registro.php?username=mudis2&password=Kazo13') */
+    fetch(`https://kuramadev.com/IniciarSesion.php?username=${username}&password=${password}`)
+      .then(response => response.text())
+      .then(data => {
+        if(data == "0")
+          /* console.log("Usuario no encontrado"); */
+        Alert.alert("Error", "Credenciales erroneas")
+        else
+          navegacion();
+      })
+    
+      .catch(error => {
+        console.warn('error', error);
+      });
+  }
 
   return (
     <ScrollView style={stylesAppTheme.scrollViewStyle}>
@@ -18,10 +41,10 @@ export const LogIn = () => {
 
         <Text style={stylesAppTheme.title}>Animebox</Text>
 
-        <TextInput style={stylesAppTheme.textInput} placeholder='Email' />
-        <TextInput style={stylesAppTheme.textInput} placeholder='Password' />
+        <TextInput style={stylesAppTheme.textInput} placeholder='Username' value={username} onChangeText={setUsername} />
+        <TextInput style={stylesAppTheme.textInput} placeholder='Password' value={password} onChangeText={setPassword} />
 
-        <TouchableOpacity style={stylesAppTheme.button} onPress={() => { navigation.navigate("BottomTabNavigator"); }}>
+        <TouchableOpacity style={stylesAppTheme.button} onPress={IniciarSesion}>
           <Text style={stylesAppTheme.textButton}>Iniciar Sesion</Text>
         </TouchableOpacity>
         <TouchableOpacity style={stylesAppTheme.buttonLink} onPress={() => { navigation.navigate("SignIn"); }}>
