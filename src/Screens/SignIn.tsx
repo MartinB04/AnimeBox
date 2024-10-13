e93d25ac79e36578290fd76a468f146de4380c4c
MartinB04 <martin.bibiano4@outlook.com>
2024-10-13 08:47:07 -0600

    Change: Registro de email y password agregado.
    [SignIn.tsx]: Actualizado.
    *Ahora se puede registrar un usuario en la base de datos.
    *De momento solo se pueden insertar los datos de email y password.

--- src/Screens/SignIn.tsx
+++ src/Screens/SignIn.tsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, StyleSheet, TextInput, Alert } from 'react-native'
 import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
 import { stylesAppTheme } from '../Theme/AppTheme';
 
 export const SignIn = () => {
     /*  const dominio = "1.0.2.2/proyecto/"; */
-    const dominio = "10.0.2.2/proyecto/";
+    /* const dominio = "10.0.2.2/proyecto/"; */
 
+const [username, setUsername] = useState('');
+const [password, setPassword] = useState('');
 
     const Registrar = () => {
-        fetch('https://kuramadev.com/Registro.php?username=mudis2&password=Kazo13')
+        /* fetch('https://kuramadev.com/Registro.php?username=mudis2&password=Kazo13') */
+        fetch(`https://kuramadev.com/Registro.php?username=${username}&password=${password}`)
             .then(response => response.text())
             .then(data => {
                 console.log('success', data);
@@ -23,8 +26,8 @@ export const SignIn = () => {
         <ScrollView>
 
             <TextInput style={stylesAppTheme.textInput} placeholder='Name' />
-            <TextInput style={stylesAppTheme.textInput} placeholder='Username' />
-            <TextInput style={stylesAppTheme.textInput} placeholder='Password' />
+            <TextInput style={stylesAppTheme.textInput} placeholder='Username' value={username} onChangeText={setUsername}/>
+            <TextInput style={stylesAppTheme.textInput} placeholder='Password' value={password} onChangeText={setPassword}/>
             <TextInput style={stylesAppTheme.textInput} placeholder='Email' />
 
             <TextInput style={stylesAppTheme.textInput} placeholder='Telefono' />
